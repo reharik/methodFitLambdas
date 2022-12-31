f40c95ac717c59e4b5692d2d7e60db1263639578
Raif Harik <harik.raif@gmail.com>
2022-12-31 13:13:28 -0600

    working on deploy

--- deploy.zsh
+++ deploy.zsh
@@ -1,11 +1,11 @@
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
 echo "****************"
 echo "Please commit changes first"
 echo "****************"
+exit 0
 fi
-
-
 
 yarn version --patch;
 git com "version bump";
 git pushme;
+serverless deploy
